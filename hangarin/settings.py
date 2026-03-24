9b5a6f8357a3c7e89a71d488f1904a9c3b5c58a8
jiyu0903 <jlouliwanag@gmail.com>
2026-03-24 12:29:17 +0800

    Add PythonAnywhere domain to ALLOWED_HOSTS

--- hangarin/settings.py
+++ hangarin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z)s7n4z_*mpwljn4mc9hkp$3_%mla6kaz6@hv7x2wtaz^o41zi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['julietluu.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
