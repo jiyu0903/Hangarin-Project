9e751893c2054c4e82d4beeb36446ab0de6d696d
jiyu0903 <jlouliwanag@gmail.com>
2026-04-07 23:33:50 +0800

    Add PWA icons and fix static files configuration

--- hangarin/settings.py
+++ hangarin/settings.py
@@ -141,9 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-import os
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
